0cb6c89f15bb4ea145f79141c07189de5544adfe
Jermaine Wates <jwates7@protonmail.com>
2023-02-08 18:53:17 -0700

    initial commit

---
+++ friends.rb
@@ -0,0 +1,16 @@
+# friends = ['johnny', 'jack', 'tom']
+ 
+# friends.each do |friends|
+#     if friends == 'johnny'
+#         puts "That's my best friend johnny"
+#     else
+#         puts "that's just my buddy #{friends}"
+#     end
+# end
+
+best_friend = "DMX"
+if best_friend == "johnny"
+    puts "good to see you bestie"
+else
+    puts "hello normal friend"
+end
